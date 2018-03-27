b63e41809a8387889e4329dc173bd6dd99d0af96
brian-mecha <brianmecha@gmail.com>
2018-03-27 22:32:45 +0300

    add api endpoints for borrow book and reset password

--- api_endpoints.py
+++ api_endpoints.py
@@ -8,10 +8,12 @@ from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, g
 
 import os
 
-##initialization
-#path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "hello-books/instance")
+# initialization
+# path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "hello-books/instance")
 
-#app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY']='\xe3\x8cw\xbdx\x0f\x9c\x91\xcf\x91\x81\xbdZ\xdc$\xedk!\xce\x19\xaa\xcb\xb7~'
 app.config['BCRYPT_LOG_ROUNDS'] = 15
@@ -24,6 +26,7 @@ jwt = JWTManager(app)
 
 books = [{"id": 1, "title": "Kamusi Ya Methali", "description": "This is A very Nice Book", "Author": "Brian Mecha"}]
 users_data = []
+borrowed_books = []
 
 blacklist = set()
 
@@ -83,7 +86,7 @@ class SingleBooksApi(MethodView):
         return abort(404, "Books Not Found")
 
 
-####all books Endpoint
+#all books Endpoint
 class BooksApi(MethodView):
     # get all books
     def get(self):
@@ -115,9 +118,8 @@ class LoginUser(MethodView):
             if len(users_email) < 1:
                 abort(401, "Wrong User Name or Password")
             else:
-                print (users_email)
-                if check_password( users_email[0]["email"],valid_user.data["email"]):
-                    access_token = create_access_token(identity=user["name"])
+                if check_password(users_email[0]["email"], valid_user.data["email"]):
+                    access_token = create_access_token(identity=user["email"])
 
                     return user, 200, {"jwt": access_token}
 
@@ -128,7 +130,6 @@ class LoginUser(MethodView):
 
         except ValidationError as err:
             abort(400, err.messages)
-
 
 
 class LogoutUser(MethodView):
@@ -152,9 +153,9 @@ class RegisterUser(MethodView):
                 abort(401, "User With Such An Email Already Exist")
 
             else:
-                userdata["password"]=set_password(userdata["password"])
+                userdata["password"] = set_password(userdata["password"])
                 users_data.append(userdata)
-                access_token = create_access_token(identity=userdata["name"])
+                access_token = create_access_token(identity=userdata["email"])
 
                 return jsonify(userdata), 200, {"jwt": access_token}
 
@@ -167,8 +168,47 @@ class RegisterUser(MethodView):
 
 
 class ResetPassword(MethodView):
+    @jwt_required
     def post(self):
-        pass
+        userdata = request.get_json()
+        try:
+            # Test if data sent is valid and has all required  fields
+            valid_user = UserSchema().load(userdata)
+
+            ####test if user trying to change password is the Authonicated user by checking identity in his token
+            if get_jwt_identity() == valid_user.data["email"]:
+                users_email = [user for user in users_data if user["email"] == valid_user.data["email"]]
+                if len(users_email) == 0:
+                    abort(401, "User Does Not Exist")
+
+                else:
+                    users_data.remove(users_email[0])
+                    valid_user.data["password"] = set_password(userdata["password"])
+
+                    users_data.append(valid_user.data)
+                    access_token = create_access_token(identity=userdata["email"])
+
+                    return jsonify(valid_user.data), 200, {"jwt": access_token}
+
+            else:
+                abort(401, "You  Are Not Authorized To Reset")
+
+        except ValidationError as err:
+            abort(401, err.messages)
+
+
+class BorrowBook(MethodView):
+    @jwt_required
+    def post(self, book_id):
+        # get user email from token
+
+        book_is_present = [book for book in books if book["id"] == id]
+        if len(book_is_present) == 0:
+            abort(404, "book Does Not Exist")
+        borrowed_book = {}
+        borrowed_book["user_email"] = get_jwt_identity()
+        borrowed_book["book_id"] = id
+        return jsonify("Book Borrowed Successfuly")
 
 
 @jwt.expired_token_loader
@@ -179,7 +219,7 @@ def my_expired_token_callback():
 
 
 def set_password(password):
-    password = bcrypt.generate_password_hash(password.encode('utf-8')).decode("utf-8")
+    password = bcrypt.generate_password_hash(password).decode('utf-8')
     return password
 
 
@@ -192,4 +232,7 @@ app.add_url_rule('/api/v1/books', view_func=BooksApi.as_view('book'))
 app.add_url_rule("/api/v1/auth/login", view_func=LoginUser.as_view('login'))
 app.add_url_rule("/api/v1/auth/register", view_func=RegisterUser.as_view('register'))
 app.add_url_rule("/api/v1/auth/logout", view_func=LogoutUser.as_view('logout'))
+app.add_url_rule("/api/v1/auth/reset", view_func=ResetPassword.as_view('reset'))
+app.add_url_rule("/api/v1/users/book/<int:book_id>", view_func=BorrowBook.as_view('borrow'))
+
 
