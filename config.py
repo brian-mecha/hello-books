5bb41daa3483432ec9f8c5f2817849b3d368d259
brian-mecha <brianmecha@gmail.com>
2018-06-14 15:15:00 +0300

    adds configuration settings

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class DevelopmentConfig(Config):
     Config settings for Development
     """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mozart@localhost:5432/hello_books'
 
 
 class TestingConfig(Config):
@@ -33,7 +33,7 @@ class ProductionConfig(Config):
     """
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mozart@localhost:5432/hello_books'
 
 
 config_app = {
